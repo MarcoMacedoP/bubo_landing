816863194d317af818046af3d57bebef21fdc7f6
Marco <marcosiegman01@gmail.com>
2020-12-19 12:35:44 -0600

    Initial project configuratiols

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    purge: [
+        // "./src/index.html"
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        fontFamily: {
+            serif: ["Century-Gothic", "serif"],
+            sans: ["SF UI", "sans"],
+        },
+        colors: {
+            dark: "#1C1F24",
+            gray: "#707070",
+            primary: "#FFB61A",
+            white: "#FFFFFF",
+        },
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
